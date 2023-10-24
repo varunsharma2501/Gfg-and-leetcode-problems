8cf16e02eda066890d0bbabbdcdf4158dfad06c4
Varun Sharma <106574953+varunsharma2501@users.noreply.github.com>
2023-10-24 20:09:08 +0530

    Create StockBuy.java
    In this problem we need to add all possible ways in which we can make profit from given stokcs

---
+++ StockBuy.java
@@ -0,0 +1,18 @@
+class Solution{
+    //Function to find the days of buying and selling stock for max profit.
+    ArrayList<ArrayList<Integer> > stockBuySell(int A[], int n) {
+        
+        ArrayList<ArrayList<Integer>> res=new ArrayList<ArrayList<Integer>>();
+        
+        for(int i=1;i<n;i++){
+            if(A[i]>A[i-1]){
+                ArrayList<Integer> ls=new ArrayList<Integer>();
+                ls.add(i-1);
+                ls.add(i);
+                res.add(ls);
+            }
+        }
+        return res;
+        // code here
+    }
+}
