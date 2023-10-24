7dba33b6e20142e917915672c05330248997a9c8
Varun Sharma <106574953+varunsharma2501@users.noreply.github.com>
2023-10-24 20:28:55 +0530

    Create MajorityEle.java

---
+++ MajorityEle.java
@@ -0,0 +1,29 @@
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int majorityElement(int arr[], int size)
+    {
+        Arrays.sort(arr);
+        int occ=1;
+        if(size==1){
+            return arr[0];
+        }
+        for(int i=1;i<size;i++){
+            if(arr[i]==arr[i-1]){
+                occ++;
+            }
+            else{
+            occ=1;    
+            }
+            
+            if(occ>size/2){
+                return arr[i];
+            }
+        }
+        return -1;
+        // your code here
+    }
+}
