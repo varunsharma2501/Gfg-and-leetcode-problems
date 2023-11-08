3757337fc475a1192f1333bff369d959ac289359
Varun Sharma <106574953+varunsharma2501@users.noreply.github.com>
2023-11-08 19:36:06 +0530

    Create TripletWith0Sum.java

---
+++ TripletWith0Sum.java
@@ -0,0 +1,28 @@
+class Solution
+{
+    // arr[]: input array
+    // n: size of the array
+    //Function to find triplets with zero sum.
+	public boolean findTriplets(int arr[] , int n)
+    {
+        Arrays.sort(arr);
+        
+        for(int i=0;i<n-2;i++){
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                if(arr[i]+arr[j]+arr[k]==0){
+                    return true;
+                }
+                else if(arr[i]+arr[j]+arr[k]>0){
+                    k--;
+                }
+                else{
+                    j++;
+                }
+            }
+            
+        }
+            return false;
+        }
+}
